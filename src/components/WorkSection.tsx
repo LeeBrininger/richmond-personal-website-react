0413d9475d24ebe19b80a9d726c0d8092ce1a2b0
Lee <richmondbrininger@gmail.com>
2025-10-30 21:08:03 -0500

    Changed from generic to work specific sections

--- src/components/WorkSection.tsx
+++ src/components/WorkSection.tsx
@@ -1,18 +1,49 @@
-import TabSection from "./TabSection.tsx";
-import {jobList} from "../data/jobList.tsx";
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Box from '@mui/material/Box';
+import {useRef, useState} from "react";
+import * as React from "react";
+import WorkTabPanel from "./WorkTabPanel.tsx";
+import type {Job} from "../types/Job.tsx";
 
-interface WorkEntryProps {
-    title: string;
-    description: string;
+
+interface WorkTabsProps {
+    jobs: Job[];
 }
 
+export default function WorkSection(props: WorkTabsProps) {
+    const {jobs} = props;
+    const [value, setValue] = useState(false);
+    const changed = useRef<boolean>(false);
 
-export default function WorkSection(props: Readonly<WorkEntryProps>) {
-    const{description} = props;
+    const handleChange = (_event: React.SyntheticEvent, newValue: any) => {
+        console.log("change");
+        changed.current = true;
+        setValue(newValue);
+    };
+
+    const handleUnselect = () => {
+        if(changed.current){
+            changed.current = false;
+        } else {
+            setValue(false);
+        }
+    };
+
     return (
-        <div className="tabContainer">
-            <h2>{description}</h2>
-            <TabSection jobs={jobList} />
-        </div>
+        <Box sx={{ width: '100%' }}>
+            <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                <Tabs value={value} onChange={handleChange} centered aria-label="basic tabs example">
+                    {jobs.map((job: Job) => (
+                        <Tab label={job.companyName} key={job.companyName} onClick={handleUnselect}/>
+                    ))}
+                </Tabs>
+            </Box>
+            {jobs.map((job: Job, index: number) => (
+                <WorkTabPanel value={value} index={index} key={index || "-1"}>
+                    {job}
+                </WorkTabPanel>
+            ))}
+        </Box>
     );
 }

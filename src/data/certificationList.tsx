5de5814100ee560c2b4b7a05d3c74a40b7ea0007
Lee <richmondbrininger@gmail.com>
2025-10-30 22:31:30 -0500

    Added certification type and data

---
+++ src/data/certificationList.tsx
@@ -0,0 +1,19 @@
+import type {Certification} from "../types/Certification.tsx";
+
+// This will be a database call in the future
+export const certificationList: Certification[] = [
+    {
+        name: "CompTIA Security+",
+        id: "COMP001022885074",
+        descriptions: [""],
+        earnedDate: "09/2025",
+        expiresDate: "09/2025",
+    },
+    {
+        name: "AWS Certified Cloud Practitioner",
+        id: "01098a92a1e84c20963846d8bfc15dfb",
+        descriptions: [""],
+        earnedDate: "10/2025",
+        expiresDate: "10/2025",
+    },
+]

183071c683491ca86c2fbbfe8a8685318c71f1f0
Lee <richmondbrininger@gmail.com>
2026-01-07 14:56:24 -0600

    Added data files

---
+++ src/data/about.tsx
@@ -0,0 +1,9 @@
+export const about: string =
+    "Senior Full-Stack Software Engineer with 7+ years " +
+    "of experience building and securing scalable web and" +
+    "desktop applications using Spring Boot, React, Core Java, " +
+    "and C++. Proven leader with experience mentoring" +
+    "engineers, supporting CI/CD pipelines and delivering secure, " +
+    "resilient systems in regulated environments." +
+    "Passionate about leveraging technology to drive impactful results" +
+    " and create production-grade software.";

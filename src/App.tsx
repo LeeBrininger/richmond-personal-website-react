c213789b42610709390c5d1e3a65e52527eb09fa
Lee <richmondbrininger@gmail.com>
2026-01-07 14:57:03 -0600

    Added header bar and formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
 import './App.css'
-import TabsSection from "./components/TabsSection.tsx";
+import './styles/Icons.css'
+import './styles/HeaderBar.css'
+import './styles/SideBar.css'
+import './styles/Tabs.css'
+import './styles/LandingPage.css'
 import Sidebar from "./components/SideBar.tsx";
-
-
+import HeaderBar from "./components/HeaderBar.tsx";
+import TabsSection from "./components/TabsSection.tsx";
 
 function App() {
-
   return (
-      <>
+      <div className="app-background">
           <Sidebar/>
-          <h1>Richmond Brininger</h1>
-          <h2>Senior Fullstack Developer</h2>
-          <TabsSection/>
-      </>
+          <div className="mainContainer">
+              <HeaderBar/>
+              <div className="cornerGoblin layer1"></div>
+              <div className="cornerGoblin layer2"></div>
+              <TabsSection/>
+          </div>
+      </div>
   )
 }
 

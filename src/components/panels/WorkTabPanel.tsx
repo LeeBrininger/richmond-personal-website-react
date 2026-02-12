6b280558db7e3bfd6bb9a5bf3adfeffcd6ce4b43
Lee <richmondbrininger@gmail.com>
2026-02-11 23:57:24 -0600

    Moved Panels into their own folder, deleted Sidebar

---
+++ src/components/panels/WorkTabPanel.tsx
@@ -0,0 +1,38 @@
+import Box from "@mui/material/Box";
+import type { Job } from "../../types/Job.tsx";
+
+interface TabPanelProps {
+  children: Job;
+  index: any;
+  value: any;
+}
+
+export default function WorkTabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`work-tabpanel-${index}`}
+      aria-labelledby={`work-tab-${index}`}
+      key={children.name}
+      {...other}
+    >
+      {value === index && (
+        <Box className="bulletList">
+          <p className="string-container">
+            <span className="left-title">{children.position}</span>
+            <span>{`${children.startDate} - ${children.endDate}`}</span>
+            <span className="right-title">{children.location}</span>
+          </p>
+          {children.descriptions.map((job, index) => (
+            <ul key={index}>
+              <li>{job}</li>
+            </ul>
+          ))}
+        </Box>
+      )}
+    </div>
+  );
+}
